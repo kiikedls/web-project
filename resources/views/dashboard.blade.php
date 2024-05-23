c287be2b993f9fe7e7ca7525f2eda610dcb0f1ca
kiike_dls <jespinozadelossantos@gmail.com>
2024-05-23 15:34:15 -0600

    primer commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,11 @@
+<x-app-layout>
+    <x-container>
+        <x-card class="mb-4">
+            {{ __("You're logged in!") }}
+        </x-card>
+
+        <x-card>
+            {{ __("You're logged in!") }}
+        </x-card>
+    </x-container>
+</x-app-layout>
